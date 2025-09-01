b9cf2fdcb41598bad5f0cb4ce2ba634f92306421
fischerjulia <100000704+fischerjulia@users.noreply.github.com>
2025-09-01 16:34:12 +0000

    Deploying to gh-pages from @ Noema-Institute/noema-institute.github.io@3d9dbd542584863c93114f36d78f0d90a1dd57a5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Noema-Institute/website",
+    "data-repo": "Noema-Institute/noema-institute.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
